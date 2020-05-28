96acff9c986c2a501fc2ba451368e728bc86d02e
pascalpoizat <pascal.poizat@lip6.fr>
2020-05-28 12:44:46 +0200

    version with a record (KO)

--- src/main/java/previews/Person.java
+++ src/main/java/previews/Person.java
@@ -1,19 +1,4 @@
 package previews;
 
-public class Person {
-    private int id;
-    private String name;
-
-    public Person(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int id() { return this.id; }
-    public String name() { return this.name; }
-
-    @Override
-    public String toString() {
-        return String.format("Person[id=%d, name=%s]", id, name);
-    }
+public record Person(int id, String name) {
 }
